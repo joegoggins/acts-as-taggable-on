3d00aed33a1966f15d7e41d9dcf440a8ee3d6403
Joe Goggins <joe.goggins@gmail.com>
2010-06-28 21:58:00 -0500

    This commit adds:   important indexes to the migration (with a lil doc about what they're for)     taggings.tagger_*  is now indexed     taggins.context is now indexed     tags.name is now indexed
      script/generate acts_as_taggable_migration now accepts "string_ids=true"
      to make taggable_id and tagger_id strings instead of integers
      handy for working with tables with non-integer primary keys

--- generators/acts_as_taggable_on_migration/acts_as_taggable_on_migration_generator.rb
+++ generators/acts_as_taggable_on_migration/acts_as_taggable_on_migration_generator.rb
@@ -1,7 +1,27 @@
 class ActsAsTaggableOnMigrationGenerator < Rails::Generator::Base 
+  def use_string_ids?
+    if (args.grep /string_ids/).empty?
+      use_string_ids = false
+      puts "
+PS: if you are integrating with tables with non-integer primary keys, you might want to do:
+  script/generate sanction string_ids=true
+instead.
+"
+    else
+      puts "
+Setting taggable_id and tagger_id to \":string, :limit => 40\", 
+don't forget to increase the limit if you need to to accomodate yer data" 
+      use_string_ids = true        
+    end
+  end
   def manifest 
     record do |m| 
-      m.migration_template 'migration.rb', 'db/migrate', :migration_file_name => "acts_as_taggable_on_migration"
+      use_string_ids = use_string_ids?
+      
+      m.migration_template 'migration.rb', 
+        'db/migrate', 
+        :migration_file_name => "acts_as_taggable_on_migration",
+        :assigns => {:use_string_ids => use_string_ids}
     end
   end
 end
--- generators/acts_as_taggable_on_migration/templates/migration.rb
+++ generators/acts_as_taggable_on_migration/templates/migration.rb
@@ -6,20 +6,34 @@ class ActsAsTaggableOnMigration < ActiveRecord::Migration
     
     create_table :taggings do |t|
       t.column :tag_id, :integer
+      <%- if use_string_ids -%>
+      t.column :taggable_id, :string, :limit => 40
+      t.column :taggable_type, :string
+            
+      t.column :tagger_id, :string, :limit => 40
+      t.column :tagger_type, :string  
+      <%- else -%>
+      
       t.column :taggable_id, :integer
+      t.column :taggable_type, :string
+            
       t.column :tagger_id, :integer
       t.column :tagger_type, :string
       
-      # You should make sure that the column created is
-      # long enough to store the required class names.
-      t.column :taggable_type, :string
+      <%- end -%>
+      
       t.column :context, :string
       
       t.column :created_at, :datetime
     end
     
-    add_index :taggings, :tag_id
-    add_index :taggings, [:taggable_id, :taggable_type, :context]
+    add_index :taggings, :tag_id # to facilitate :include => :tags eager loading (default)
+    add_index :taggings, [:taggable_id, :taggable_type, :context] # for regular lookups   
+    add_index :taggings, [:tagger_id, :tagger_type] # for ownership based lookups
+    
+    add_index :tags, :name # for making sorted lists of tags, and LIKE q% style queries
+    add_index :taggings, :context  # for direct joins against context (not thru tags)
+    
   end
   
   def self.down
